29d8d097cc78573208c65de26bf45af87d4fc03a
ebbbates <bat20033@byui.edu>
2021-02-09 19:12:27 -0700

    commit

--- lesson05/js/hideBanner.js
+++ lesson05/js/hideBanner.js
@@ -1,3 +1,11 @@
-function hideBanner {
-    
-}
+    var fullDate = new Date();
+
+    var dayOfWeek = fullDate.getDay();
+
+    if (dayOfWeek == 5) {
+        document.getElementById("pancakes").style.display="block";
+    }
+
+    else {
+        document.getElementById("pancakes").style.display="none";
+    }
